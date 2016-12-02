6885cd34d0522e156f993125bea91ac293b4dd62
kevster555999 <kevster555999@gmail.com>
2016-12-02 17:39:03 -0500

    testing some clustering

--- spectral.py
+++ spectral.py
@@ -4,7 +4,9 @@ import numpy as np
 from sklearn.feature_extraction import image
 from sklearn.cluster import SpectralClustering
 from sklearn.cluster import spectral_clustering
+from scipy import ndimage
 import matplotlib.pyplot as plt
+import pandas as pd
 
 from sklearn.cluster import KMeans
 
@@ -13,6 +15,14 @@ def laplacian(np_arr):
 	for i in np_arr:
 		break
 	pass
+
+def largest_connected_area(a):
+	for i in range(20):
+		for j in range(20):
+			if a[i][j] == 1:
+				pass
+
+
 x=[]
 y=[]
 with open('training_data.csv','r') as csvfile:
@@ -40,11 +50,32 @@ print graph
 #print sc.labels_
 labels = spectral_clustering(graph,n_clusters=2,eigen_solver='arpack')
 
-print labels.reshape(20,20)
-plt.matshow(labels.reshape(20,20))
-plt.matshow(np_x[1].reshape(20,20))
+#print labels.reshape(20,20)
+#plt.matshow(labels.reshape(20,20))
+#plt.imshow(np_x[1].reshape(20,20))
+
+#sx=ndimage.sobel(np_x[1].reshape(20,20),axis=0,mode='constant')
+#sy=ndimage.sobel(np_x[1].reshape(20,20),axis=1,mode='constant')
+#sobel=np.hypot(sx,sy)
+#for i in range(20):
+#	for j in range(20):
+#		if sobel[i][j] > 300:
+#			sobel[i][j] = 1
+#		else:
+#			sobel[i][j] = 0
+#plt.imshow(sobel)
+#print sobel
+pic=np_x[1000].reshape(20,20)
+for i in range(20):
+	for j in range(20):
+		if pic[i][j] > 127:
+			pic[i][j] = 0
+		else:
+			pic[i][j] = 1
+print pic
+plt.matshow(pic)
 plt.show()
-print chr(y[1])
+print chr(y[1000])
 #print len(labels)
 #print labels
 
